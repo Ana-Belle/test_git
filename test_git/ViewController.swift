5bd18747137009ef1cf334acb016bafad4de2de8
Anastasia Belyakova <ana-belle@mail.ru>
2021-10-18 14:27:11 +0300

    Print hay

--- test_git/ViewController.swift
+++ test_git/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello!")
+        print("How are you?")
         // Do any additional setup after loading the view.
     }
 
